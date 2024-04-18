885021cd1299630312f0d5951cfc5f8814438b80
Negar Babaei <negarbabaei377@gmail.com>
2024-04-18 15:08:24 +0330

    feat: list and login page created

---
+++ src/pages/list/List.jsx
@@ -0,0 +1,10 @@
+import {DataTable} from "../../components";
+import {Layout} from "../../layout";
+
+export const List = () => {
+    return (
+        <Layout>
+            <DataTable/>
+        </Layout>
+    );
+};
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,8 @@
+export const Login = () => {
+    return (
+        <div>
+            this is login page
+        </div>
+    );
+};
+
